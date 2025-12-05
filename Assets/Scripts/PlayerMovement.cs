bfe7f5aaf8f85c7140bfb73e7032d396656a34f6
kido <yunkido777@gmail.com>
2025-12-05 16:27:33 +0900

    Fiip player when moving left or right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,9 +14,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if(Input.GetKey(KeyCode.Space))
+        //Fiip player when moving left or right
+        if (horizontalInput> 0.01f)
+            transform.localScale = Vector3.one;
+        else if(horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);        
     }
 }

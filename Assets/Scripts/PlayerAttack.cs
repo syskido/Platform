b88dc303ac0524b610ed00c894b9cec5a3233d39
kido <yunkido777@gmail.com>
2025-12-05 21:39:09 +0900

    Attack 성공

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+
+    private Animator animator;
+    private PlayerMovement playerMovement;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMovement.canAttack())
+           Attack();
+
+        cooldownTimer += Time.deltaTime;
+    }
+    private void Attack()
+    {
+        animator.SetTrigger("attack");
+        cooldownTimer = 0;
+
+        fireballs[0].transform.position = firePoint.position;
+        fireballs[0].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+        //pool fireballs 
+    }
+}
